2d0721aa78952b901f06b91a5752298966d474cb
rubin <safedupkid@googlemail.com>
2020-07-16 12:59:36 +0200

    Added styling and server code

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const exp = require('express')
 const app = exp()
-
 app.set('view engine', 'ejs')
 app.use(exp.static('public'))
 app.get('/', (req, res) => {
-	res.send('Greetings')
+	res.render('index')
 })
 
 //Listens on port 8080
 server = app.listen(8080)
+
+const io = require('socket.io')(server)
+io.on('connection', (socket) => {
+	consoler.log('Connection Established')
+})
+
