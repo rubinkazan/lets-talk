bd1be5be5856597bff36657e688c2a71977a3438
rubin <safedupkid@googlemail.com>
2020-07-16 13:29:33 +0200

    Added styling and ability for users to change user name

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ server = app.listen(8080)
 
 const io = require('socket.io')(server)
 io.on('connection', (socket) => {
-	consoler.log('Connection Established')
+	console.log('Connection Established')
+
+socket.username = 'Default-User'
+socket.on('set_username', (data) => {
+	socket.username = data.username
 })
 
+socket.on('new_message',(data) => {
+	io.sockets.emit('new_message', {message: data.message, username:socket.username});
+})
+
+socket.on('typing', (data) => {
+	socket.broadcast.emit('typing', {username: socket.username})
+})
+})
